b2abc79ff8d11a4813e70b0a7a4299031a0944f9
ogorei <reinasuzuki853@gmail.com>
2023-01-20 12:04:56 +0900

    updated module

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,5 +1,8 @@
 import testimonials from "./testimonials"
+import works from "./works"
+import skills from "./skills"
+import abouts from "./abouts"
 
-export const schemaTypes = [testimonials]
+export const schemaTypes = [testimonials, works, skills, abouts]
 
 
